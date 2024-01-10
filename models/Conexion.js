d4688140fa0caeae1ef442a8034850b17c40c86f
JavierMorales <jmoralesr@protonmail.com>
2024-01-10 11:10:20 +0100

    Solucionado pool de conexiones

--- models/Conexion.js
+++ models/Conexion.js
@@ -1,61 +1,41 @@
 const mysql = require('mysql2');
 
 class Conexion {
+    
     constructor(options) {
-        this.connection = mysql.createConnection({
+        this.config = {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
+            connectionLimit: process.env.DB_MAXCONNECTIONS, //Por defecto son 10.
             port: process.env.DB_PORT
-        });
-    }
-
-    conectar = () => {
-        this.connection.connect( (err) => {
-            if (err) {
-                console.error('Error de conexion: ' + err.stack);
-                return;
-            }
-            console.log('Conectado con el identificador ' + this.connection.threadId);
-        });
-    }
-
-    desconectar = () => {
-        this.connection.end( (err) => {
-            if (err) {
-                console.error('Error de conexion: ' + err.stack);
-                return;
-            }
-        console.log('Desconectado con éxito');
-        });
+        };
+        this.pool = mysql.createPool(this.config);
     }
     
-    query = ( sql, values ) => {
+    query = (sql, values) => {
         //Devolver una promesa
-        return new Promise(( resolve, reject ) => {
-            this.connection.query(sql, values, ( err, rows) => {
-                if ( err ) {
-                    reject( err )
+        return new Promise((resolve, reject) => {
+            this.pool.query(sql, values, (err, rows) => {
+                if (err) {
+                    reject(err)
                 } else {
                     // console.log('Llego aquí');
                     if (rows.length === 0) {
                         reject(err);
                     }
-                    resolve( rows )
+                    resolve(rows)
                 }
-                })
             })
-        }
+        })
+    }
     
     insertarTarea = async(descripcion, duracion, dificultad, realizada) => {
         let resultado = 0;
-        this.conectar();
         try {
             resultado = await this.query('INSERT INTO tareas VALUES (?, ?, ?, ?, ?)', [null, duracion, dificultad, realizada, descripcion]);
-            this.desconectar();
         } catch (error) {
-            this.desconectar();
             throw error;
         }
         return resultado;
@@ -63,12 +43,9 @@ class Conexion {
 
     getTareas = async () => {
         let resultado = []
-        this.conectar();
         try {
             resultado = await this.query('SELECT * FROM tareas')
-            this.desconectar();
         } catch (error) {
-            this.desconectar();
             throw error;
         }
         return resultado;
@@ -76,12 +53,9 @@ class Conexion {
 
     deleteTarea = async (id) => {
         let resultado = 0
-        this.conectar();
         try {
             resultado = await this.query('DELETE FROM tareas WHERE id = ?', [id])
-            this.desconectar();            
         } catch (error) {
-            this.desconectar();
             throw error;
         }
         return resultado;
@@ -89,13 +63,10 @@ class Conexion {
 
     updateTarea = async (id, descripcion, duracion, dificultad, realizada) => { 
         let resultado = 0
-        this.conectar();
         try {
             resultado = await this.query(`UPDATE tareas SET descripcion = ?, duracion = ?, dificultad = ?, realizada = ? WHERE id = ?`, 
             [descripcion, duracion, dificultad, realizada, id]);
-        this.desconectar(); 
         } catch (error) {
-            this.desconectar();
             throw error;
         }
         return resultado;
